65ae94d6228615e5ff22fb46e3831b7027c1291e
Ankur <ankurveer0522@gmail.com>
2023-02-21 17:32:25 +0530

    Added code for movie review application

--- src/main/java/com/project/movies/controllers/MovieController.java
+++ src/main/java/com/project/movies/controllers/MovieController.java
@@ -7,10 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +18,7 @@ public class MovieController {
 
     @Autowired
     private MovieService movieService;
+    @CrossOrigin(origins = "http://localhost:3000") // for removing CORS errors, by default it allows all origins (front end running on 3000 port)
     @GetMapping("/all")
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>( movieService.findAllMovies(), HttpStatus.OK);
@@ -31,6 +29,7 @@ public class MovieController {
         return new ResponseEntity<Movie>(movieService.findMovieById(id), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/imdb/{imdbId}")
     public ResponseEntity<Optional<Movie>> getMovieByImdbId(@PathVariable("imdbId") String imdbId) throws MovieNotFoundException {
         return new ResponseEntity<Optional<Movie>>(movieService.findMovieByImdbId(imdbId), HttpStatus.OK);
--- src/main/java/com/project/movies/controllers/ReviewController.java
+++ src/main/java/com/project/movies/controllers/ReviewController.java
@@ -6,10 +6,7 @@ import com.project.movies.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -20,6 +17,7 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/create")
     public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload) throws MovieNotFoundException {
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("imdbId"),payload.get("reviewBody")),
